4fb4886f2b4775a8aaa9c9577a3c3ac4bb724bbf
BoLoMi <ao5057@163.com>
2020-02-26 21:41:23 +0800

    init repo

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {   //项目启动类
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
